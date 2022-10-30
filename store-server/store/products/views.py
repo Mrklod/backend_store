c41e2cbef6757fcfedcbcb4b6f10197b7ca8e892
Sasha Osin <sashaosin364@gmail.com>
2022-10-30 18:23:12 +0200

    Add project

---
+++ store-server/store/products/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+from products.models import ProductCategory,Product
+# Create your views here.
+# контролеры = views = функции
+
+def index(request):
+    context = {
+        'title':'Geekshop'
+    }
+    return render(request, "products/index.html",context)
+
+def products(request):
+    context = {
+        'title':'Catalog',
+        'categories': ProductCategory.objects.all(),
+        'products':Product.objects.all()
+    }
+    return render(request, "products/products.html",context)
+
