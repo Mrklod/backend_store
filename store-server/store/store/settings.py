a7162c0791785ced4ac16cd605a6844ba39a2f07
Sasha Osin <sashaosin364@gmail.com>
2022-11-01 14:08:27 +0200

    Create users app

--- store-server/store/store/settings.py
+++ store-server/store/store/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     "products",
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -125,9 +126,11 @@ STATICFILES_DIRS = (
 MEDIA_URL="/media/"
 MEDIA_ROOT= os.path.join(BASE_DIR,"media")
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+AUTH_USER_MODEL= 'users.User'
 
