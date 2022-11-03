50001a6bf0016157110082b9f3049e8f7ff64677
Sasha Osin <sashaosin364@gmail.com>
2022-11-03 16:07:33 +0200

    Add working register

--- store-server/store/users/forms.py
+++ store-server/store/users/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm,UserCreationForm
 from django import forms
 
 from users.models import User
@@ -16,5 +16,24 @@ class UserLoginForm(AuthenticationForm):
 
     # def __int__(self,*args,**kwargs):
     #     super(UserLoginForm,self).__init__(*args,**kwargs)
+    #     for field_name , field in self.fields.items():
+    #         field.widget.attrs['class'] = 'form-control py-4'
+
+
+
+
+class UserRegistationForm(UserCreationForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-4','placeholder': 'Введите имя'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-4','placeholder': 'Введите фамилию'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-4','placeholder': 'Введите имя пользователя'}))
+    email = forms.CharField(widget=forms.EmailInput(attrs={'class': 'form-control py-4','placeholder': 'Введите адрес электонной почты'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control py-4','placeholder': 'Введите пароль'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control py-4','placeholder': 'Повторите пароль'}))
+    class Meta:
+        model = User
+        fields = ('first_name','last_name','username','email','password1','password2')
+
+    # def __int__(self,*args,**kwargs):
+    #     super(UserRegistationForm,self).__init__(*args,**kwargs)
     #     for field_name , field in self.fields.items():
     #         field.widget.attrs['class'] = 'form-control py-4'
--- store-server/store/users/views.py
+++ store-server/store/users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,HttpResponseRedirect
 from django.urls import reverse
 from django.contrib import auth
 
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm,UserRegistationForm
 
 def login(request):
     if request.method == 'POST':
@@ -20,4 +20,15 @@ def login(request):
     return render(request,'users/login.html',context)
 
 def register(request):
-    return render(request,'users/register.html')
+    if request.method == 'POST':
+        form = UserRegistationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:login'))
+        else:
+            print(form.errors)
+    else:
+        form = UserRegistationForm()
+    form = UserRegistationForm()
+    context = {'form':form}
+    return render(request,'users/register.html',context)
